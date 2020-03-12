f1248a4832c64abbbd072d928ba77075a7969214
大島卓也 <takuyaoshima@1stbit-MacBook-Pro.local>
2020-03-12 13:44:51 +0900

    プルリク練習

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/html"
+css_dir = "/html/css"
+sass_dir = "/html/sass"
+images_dir = "/html/img/"
+javascripts_dir = "/html/js/"
+output_style = :expanded
+line_comments = false
+relative_assets = true
+Encoding.default_external = "utf-8"
